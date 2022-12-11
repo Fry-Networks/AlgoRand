0ad669b0b6d12d94fa3781b197ec1f6858983810
Simon <44496148+siim0n@users.noreply.github.com>
2022-12-11 20:49:46 +0100

    fixes and add multiple addresses

--- main.ts
+++ main.ts
@@ -1,60 +1,43 @@
 const token = '';
-const server = 'https://testnet-api.algonode.cloud';
+const server = 'https://mainnet-api.algonode.network';
 const port = 443;
 import * as algosdk from 'algosdk';
 const client = new algosdk.Algodv2(token, server, port);
-import fs from 'fs';
-
+import config from './config.json';
+//open the xlsx file and read the data
+import * as XLSX from 'xlsx';
+const workbook = XLSX.readFile(config.excel_file_name);
+const sheet_name_list = workbook.SheetNames;
+const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+//get the addresses from the xlsx file
+//get the name of the highest row of the 3rd column
+const addresses: string[] = [];
+xlData.forEach((row: any) => {
+  addresses.push(row[config.addresses_column_name]);
+});
 (async () => {
   console.log(await client.status().do());
-  //const account = algosdk.mnemonicToSecretKey("Your mnemonic");
-  //if there is no account.json file, create one with 2 accounts
-  let addresses: string[] = [];
-  let accounts: algosdk.Account[] | { addr: string, sk: Object }[] = [];
-  if (!fs.existsSync('accounts.json')) {
-    const testAccount = algosdk.generateAccount();
-    console.log(testAccount);
-    const testAccount2 = algosdk.generateAccount();
-    //store the accounts in a json file
-    const accountsToSave = [testAccount, testAccount2];
-
-    fs.writeFile('accounts.json', JSON.stringify(accountsToSave), function (err: any) {
-      if (err) throw err;
-      console.log('Saved!');
-    });
-    addresses = [testAccount.addr, testAccount2.addr];
-    accounts = [testAccount, testAccount2];
-  } else {
-    const accountsRead: { addr: string, sk: Object }[] = JSON.parse(fs.readFileSync('accounts.json', 'utf8'));
-    addresses = [accountsRead[0].addr, accountsRead[1].addr];
-    //update the accounts array with valid accounts from accountsRead
-    //convert the secret keys to Uint8Array
-    accountsRead[0].sk = Uint8Array.from(Object.values(accountsRead[0].sk));
-    accountsRead[1].sk = Uint8Array.from(Object.values(accountsRead[1].sk));
-    accounts = accountsRead;
-  }
-  console.log(addresses, accounts);
-  //addresses is an array of addresses, send the same amount to each address of FrysCrypto (FRY) which has a contract number: 924268058
-  //the amount is 1 Algo
-  const amount = 1000000;
+  const account = algosdk.mnemonicToSecretKey(config.main_account_mnemonic);
+  //send the same amount to each address of FrysCrypto (FRY) which has a contract number: 924268058
+  const amount = config.amount_in_microAlgos;
   const enc = new TextEncoder();
-  const note = enc.encode("FrysCrypto reward");
+  const note = enc.encode(config.note_to_send);
   const params = await client.getTransactionParams().do();
-  const txn = algosdk.makePaymentTxnWithSuggestedParams(
-    addresses[0],
-    addresses[1],
-    amount,
-    undefined,
-    note,
-    params,
+  for (const address of addresses) {
+  const txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+      from: account.addr,
+      to: address,
+      amount: amount,
+      assetIndex: config.asset_index,
+      note: note,
+      suggestedParams: params,
+    }
   );
   //convert the account sk object to Uint8Array
-  //@ts-ignore
-  const signedTxn = txn.signTxn(accounts[0].sk);
+  const signedTxn = txn.signTxn(account.sk);
   const tx = (await client.sendRawTransaction(signedTxn).do());
   console.log("Transaction : " + tx.txId);
-  console.log(tx)
-
+  }
 })().catch((e) => {
   console.log(e);
 });
